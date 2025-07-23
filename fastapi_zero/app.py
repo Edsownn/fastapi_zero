4a05c2cd26cefb16e7e58c335ea8c24956d1f64a
Edsownn <albertoprj1@gmail.com>
2025-07-23 15:18:48 -0300

    Adicionando schema de mensagem, e exercicio2- retornar ola mundo usando html e testando

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
 from fastapi import FastAPI
-
+from fastapi_zero.schemas import Message
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.get('/exercicio-HTML', response_class=HTMLResponse)
+def exercicio_html():
+    return """
+    <html>
+        <head>
+            <title>Exercício HTML</title>
+        </head>
+        <body>
+            <h1>Ola Mundo</h1>
+        </body>
+    </html>
+    """
